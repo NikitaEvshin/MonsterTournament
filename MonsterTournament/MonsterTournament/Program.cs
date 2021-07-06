08593be7c51046a24039ae3b6752f2689fccf127
Your Name <you@example.com>
2021-07-06 20:30:12 +0300

    add project

---
+++ MonsterTournament/MonsterTournament/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonsterTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
