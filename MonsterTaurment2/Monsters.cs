e4f52a3b5dcfe43cf81e7405cd1f920562c8119e
Your Name <you@example.com>
2021-07-25 18:57:06 +0300

    made new version

---
+++ MonsterTaurment2/Monsters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTaurment2
+{
+    public class Monsters
+    {
+        public string Name;
+        public int Hp;
+        public int Dmg;
+        public bool IsAlive;
+
+        public Monsters(string name, int hp, int dmg, bool isAlive)
+        {
+            Name = name;
+            Hp = hp;
+            Dmg = dmg;
+            IsAlive = isAlive;
+        }
+
+        public void GetDamage(int Dmg)
+        {
+            Hp = Hp - Dmg;
+            if (Hp < 0)
+            {
+                IsAlive = false;
+            }
+        }
+        public override string ToString()
+        {
+            return "Имя " + Name;
+        }
+    }
+}
---
+++ MonsterTaurment2/Pair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTaurment2
+{
+    public class Pair
+    {
+        public List<Monsters> FirstSelection;
+
+        public Pair(List<Monsters> firstSelection)
+        {
+            FirstSelection = firstSelection;
+        }
+
+        public static (List<Monsters> firsthalf, List<Monsters> secondhalf) Selection(List<Monsters> monster)
+        {
+            List<Monsters> firsthalf = new List<Monsters>();
+            List<Monsters> secondhalf = new List<Monsters>();
+
+            for (int i = 0; i < monster.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    firsthalf.Add(monster[i]);
+                }
+                else
+                {
+                    secondhalf.Add(monster[i]);
+                }
+            }
+            return (firsthalf, secondhalf);
+        }
+    }
+}
